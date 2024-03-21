625dd15f026f25130a3eee6c05f40d2eb9f38073
ecenuracar <ece.acar@theinfatuation.com>
2024-03-21 20:53:34 +0300

    first commit

---
+++ src/test/java/com/example/test/MainPageTest.java
@@ -0,0 +1,73 @@
+package com.example.test;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class MainPageTest {
+    private WebDriver driver;
+    private MainPage mainPage;
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+      //  System.setProperty("webdriver.chrome.driver", "src/test/drivers/chromedriver.exe");
+        driver = new ChromeDriver(options);
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.jetbrains.com/");
+
+        mainPage = new MainPage(driver);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void search() {
+        mainPage.searchButton.click();
+
+        WebElement searchField = driver.findElement(By.cssSelector("[data-test='search-input']"));
+        searchField.sendKeys("Selenium");
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button[data-test='full-search-button']"));
+        submitButton.click();
+
+        WebElement searchPageField = driver.findElement(By.cssSelector("input[data-test='search-input']"));
+        assertEquals("Selenium", searchPageField.getAttribute("value"));
+    }
+
+    @Test
+    public void toolsMenu() {
+        mainPage.toolsMenu.click();
+
+        WebElement menuPopup = driver.findElement(By.cssSelector("div[data-test='main-submenu']"));
+        assertTrue(menuPopup.isDisplayed());
+    }
+
+    @Test
+    public void navigationToAllTools() {
+        mainPage.seeAllToolsButton.click();
+
+        WebElement productsList = driver.findElement(By.id("products-page"));
+        assertTrue(productsList.isDisplayed());
+        assertEquals("All Developer Tools and Products by JetBrains", driver.getTitle());
+    }
+}
